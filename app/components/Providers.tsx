1fba8bea01d1605d1378638c24a81dfd642a9ba7
boeghh-gif <boghdesign@gmail.com>
2023-11-12 00:39:23 +0100

    fixes

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({children}: {children: ReactNode}) {
             mode='payment'
             cartMode='client-only'
             stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-            successUrl='http://localhost:3000/success'
-            cancelUrl='http://localhost:3000/error'
+            successUrl='http://ecommerce-git-main-rasmus-boghs-projects.vercel.app/stripe/success'
+            cancelUrl='http://ecommerce-git-main-rasmus-boghs-projects.vercel.app/stripe/error'
             currency='USD'
             billingAddressCollection={false}
             shouldPersist={true}
