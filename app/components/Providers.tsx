51bcdcf1b0ac8e9c739bc6c43c83483e80614867
boeghh-gif <boghdesign@gmail.com>
2023-11-12 00:41:41 +0100

    changed provider urls

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({children}: {children: ReactNode}) {
             mode='payment'
             cartMode='client-only'
             stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-            successUrl='http://ecommerce-git-main-rasmus-boghs-projects.vercel.app/stripe/success'
-            cancelUrl='http://ecommerce-git-main-rasmus-boghs-projects.vercel.app/stripe/error'
+            successUrl='https://ecommerce-six-gilt.vercel.app/stripe/success'
+            cancelUrl='https://ecommerce-six-gilt.vercel.app/stripe/error'
             currency='USD'
             billingAddressCollection={false}
             shouldPersist={true}
